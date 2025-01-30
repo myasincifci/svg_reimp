6396fbc22eda8aebd68e6a1b1beb5028b924f792
Yasin Cifci <myasincifci@gmail.com>
2025-01-30 16:37:37 +0100

    cleaned up

--- models/svg.py
+++ models/svg.py
@@ -13,72 +13,50 @@ class SVG_Deterministic(pl.LightningModule):
         self.decoder = decoder(128, skip=cfg.skip)
         self.cfg = cfg
 
-    # def forward(self, x):
-        
-        
-    #     return mse
+    def forward(self, x):
+        # initialize the hidden state.
+        self.lstm.hidden = self.lstm.init_hidden()
+
+        x = x.permute((1,0,2,3))
+        T, B, H, W = x.size()
+
+        h_seq = [self.encoder(x[t][:,None]) for t in range(T)]
+
+        x_preds_past = []
+        x_preds_future = []
+        h_pred = None
+        for i in range(1, self.cfg.n_past+self.cfg.n_future):
+            if i <= self.cfg.n_past:	
+                h, skip = h_seq[i-1]
+            else:
+                h = h_pred # h_seq[i-1][0]
+
+            if self.cfg.vf_skip:
+                s = h if i == 1 else h_pred
+                h_pred = self.lstm(h) + s
+            else:
+                h_pred = self.lstm(h)
+
+            # predictions [x_1_hat, ..., x_T_hat], T = n_past + n_future - 1
+            if i < self.cfg.n_past:
+                x_preds_past.append(self.decoder([h_pred, skip]))
+            else:
+                x_preds_future.append(self.decoder([h_pred, skip]))
+
+        return torch.stack(x_preds_past, dim=0).permute((1,0,2,3,4)), torch.stack(x_preds_future, dim=0).permute((1,0,2,3,4))
 
     def training_step(self, batch, batch_idx):
-        x = batch
+        x_preds_past, x_preds_future = self(batch) 
+        loss = F.mse_loss(x_preds_future.squeeze(), batch[:,self.cfg.n_past:], reduction='none').mean(dim=(0,2,3)).sum()
+        self.log('train/loss', loss, prog_bar=True)
         
-        # initialize the hidden state.
-        self.lstm.hidden = self.lstm.init_hidden()
-
-        x = x.permute((1,0,2,3))
-        T, B, H, W = x.size()
-
-        h_seq = [self.encoder(x[t][:,None]) for t in range(T)]
-
-        mse = 0
-        for i in range(1, self.cfg.n_past+self.cfg.n_future):
-            if i <= self.cfg.n_past:	
-                h, skip = h_seq[i-1]
-            else:
-                h = h_pred # h_seq[i-1][0]
-            s = h if i == 1 else h_pred
-
-            if self.cfg.vf_skip:
-                h_pred = self.lstm(h) + s
-            else:
-                h_pred = self.lstm(h)
-
-            if i >= self.cfg.n_past:
-                x_pred = self.decoder([h_pred, skip])
-                mse += F.mse_loss(x_pred.squeeze(), x[i])
-        
-        self.log('train/loss', mse, prog_bar=True)
-        
-        return mse
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        x = batch
-        
-        # initialize the hidden state.
-        self.lstm.hidden = self.lstm.init_hidden()
+        x_preds_past, x_preds_future = self(batch) 
+        loss = F.mse_loss(x_preds_future.squeeze(), batch[:,self.cfg.n_past:], reduction='none').mean(dim=(0,2,3)).sum()
 
-        x = x.permute((1,0,2,3))
-        T, B, H, W = x.size()
-
-        h_seq = [self.encoder(x[t][:,None]) for t in range(T)]
-
-        mse = 0
-        for i in range(1, self.cfg.n_past+self.cfg.n_future):
-            if i <= self.cfg.n_past:	
-                h, skip = h_seq[i-1]
-            else:
-                h = h_pred # h_seq[i-1][0]
-            s = h if i == 1 else h_pred
-
-            if self.cfg.vf_skip:
-                h_pred = self.lstm(h) + s
-            else:
-                h_pred = self.lstm(h)
-
-            if i >= self.cfg.n_past:
-                x_pred = self.decoder([h_pred, skip])
-                mse += F.mse_loss(x_pred.squeeze(), x[i])
-
-        self.log('val/loss', mse, prog_bar=True)
+        self.log('val/loss', loss, prog_bar=True)
 
     # def on_validation_epoch_end(self):
     #     sample = self.trainer.datamodule.val_dataloader().dataset[0]
