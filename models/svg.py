8a5aa266e511d07a503d28e7ee035024ab8424c1
Yasin Cifci <myasincifci@gmail.com>
2025-01-31 12:37:54 +0100

    added plotting

--- models/svg.py
+++ models/svg.py
@@ -5,6 +5,8 @@ import pytorch_lightning as pl
 from .modules.dcgan_64 import encoder, decoder
 from .modules.lstm import lstm
 
+from torchvision.utils import make_grid
+
 class SVG_Deterministic(pl.LightningModule):
     def __init__(self, cfg):
         super(SVG_Deterministic, self).__init__()
@@ -60,37 +62,15 @@ class SVG_Deterministic(pl.LightningModule):
 
         self.log('val/loss', loss, prog_bar=True)
 
-    # def on_validation_epoch_end(self):
-    #     sample = self.trainer.datamodule.val_dataloader().dataset[0]
-    #     x = sample.unsqueeze(0)
+    def on_validation_epoch_end(self):
+        sample = torch.from_numpy(self.trainer.datamodule.val_dataloader().dataset[0]).to(self.device)
+        x = sample.unsqueeze(0)
+        x = x.repeat(100, 1, 1, 1) # TODO: hack, remove later
         
-    #     # initialize the hidden state.
-    #     self.lstm.hidden = self.lstm.init_hidden()
+        x_preds_past, x_preds_future = self(x)    
 
-    #     x = x.permute((1,0,2,3))
-    #     T, B, H, W = x.size()
-
-    #     h_seq = [self.encoder(x[t][:,None]) for t in range(T)]
-
-    #     predictions = []
-    #     for i in range(1, self.cfg.n_past+self.cfg.n_future):
-    #         if i <= self.cfg.n_past:	
-    #             h, skip = h_seq[i-1]
-    #             s = h
-    #         else:
-    #             h = h_pred # h_seq[i-1][0]
-
-    #         if self.cfg.vf_skip:
-    #             h_pred = self.lstm(h) + s
-    #         else:
-    #             h_pred = self.lstm(h)
-
-    #         if i >= self.cfg.n_past:
-    #             x_pred = self.decoder([h_pred, skip])
-    #             predictions.append(x_pred.squeeze().cpu())
-
-    #     predictions = torch.stack(predictions, dim=0)
-    #     self.logger.experiment.add_images('val/sample_predictions', predictions, self.current_epoch)
+        # self.logger.log({"val/sample_predictions": [Image(x_preds_future, caption="Sample Predictions")]}, step=self.current_epoch)
+        self.logger.log_image('val/sample_predictions', [make_grid(x_preds_future[0], nrow=10)], self.current_epoch)
         
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.cfg.param.lr)
